432df88e65329373b05150d9b629c8389d4e11f4
simonbesch <simonbescheron@gmail.com>
2024-03-26 17:41:12 +0100

    react basic 3

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,51 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/150.png",
+    },
+    {
+      name: "palkia",
+      
+    },
+  ];
+
 
 function PokemonCard() {
-    return(
-    <>
+
+  const imgOrNot = (pokemon) => {
+      if (pokemon.imgSrc !== undefined){
+        return(
+          <img src={pokemon.imgSrc} alt={"pas d'image affiché pour " + pokemon.name} />
+        )
+      }else{
+        return(
+          <p>???</p>
+        )
+      }
+  }
+
+    return (
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
+          {pokemonList.map((pokemon) => (
+            <div key={pokemon.name}>
+                <p>{pokemon.name}</p>
+                {imgOrNot(pokemon)}
+                {/* {pokemon.imgSrc !== undefined ? <img src={pokemon.imgSrc} alt={"pas d'image affiché pour " + pokemon.name} /> : <p>???</p>} */}
+            </div>
+          ))}
         </figure>
-    </>
-    )
+      );
+
+
+
+
+    
 }
 
 export default PokemonCard;
