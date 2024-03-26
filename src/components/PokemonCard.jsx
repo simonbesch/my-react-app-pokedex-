e0a3dd3edf9ebd1badde50e2d2fb3174f6c9c484
simonbesch <simonbescheron@gmail.com>
2024-03-26 15:27:04 +0100

    react basic 2

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,12 @@
+
+function PokemonCard() {
+    return(
+    <>
+        <figure>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
+        </figure>
+    </>
+    )
+}
+
+export default PokemonCard;
