193e94cfb1eb765a0e1d19994648f31b22d42cb6
simonbesch <simonbescheron@gmail.com>
2024-03-27 16:41:22 +0100

    react basic 4 ( pas fait exatement pareil )

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,35 @@
 import PokemonCard from "./components/PokemonCard.jsx";
 import './App.css'
 
-function App() {
-  // const [count, setCount] = useState(0)
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/150.png",
+  },
+  {
+    name: "palkia",
+    
+  },
+];
 
+
+
+function App() {
   return (
     <>
-      <PokemonCard />
+      {pokemonList.map((pokemon) => (
+        <PokemonCard 
+        key={pokemon.name}
+        nom={pokemon.name}
+        img={pokemon.imgSrc}
+        />
+      ))}
     </>
   )
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,51 +1,42 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/150.png",
-    },
-    {
-      name: "palkia",
-      
-    },
-  ];
 
 
-function PokemonCard() {
 
-  const imgOrNot = (pokemon) => {
-      if (pokemon.imgSrc !== undefined){
-        return(
-          <img src={pokemon.imgSrc} alt={"pas d'image affiché pour " + pokemon.name} />
-        )
-      }else{
-        return(
-          <p>???</p>
-        )
-      }
-  }
-
-    return (
-        <figure>
-          {pokemonList.map((pokemon) => (
-            <div key={pokemon.name}>
-                <p>{pokemon.name}</p>
-                {imgOrNot(pokemon)}
-                {/* {pokemon.imgSrc !== undefined ? <img src={pokemon.imgSrc} alt={"pas d'image affiché pour " + pokemon.name} /> : <p>???</p>} */}
-            </div>
-          ))}
-        </figure>
-      );
+function PokemonCard({nom, img}) {
+  return(
+    <>
+      <p>Pokemon: {nom}</p>
+      {img !== undefined ? <img src={img} alt={"pas d'image affiché pour " + nom} /> : <p>???</p>}
+      {/* <img src={img} alt={nom} /> */}
+    </>
+  )
 
 
 
 
-    
+
+  // const imgOrNot = (pokemon) => {
+  //     if (pokemon.imgSrc !== undefined){
+  //       return(
+  //         <img src={pokemon.imgSrc} alt={"pas d'image affiché pour " + pokemon.name} />
+  //       )
+  //     }else{
+  //       return(
+  //         <p>???</p>
+  //       )
+  //     }
+  // }
+    // return (
+    //     <figure>
+    //       {pokemonList.map((pokemon) => (
+    //         <div key={pokemon.name}>
+    //             <p>{pokemon.name}</p>
+    //             {imgOrNot(pokemon)}
+    //             {/* {pokemon.imgSrc !== undefined ? <img src={pokemon.imgSrc} alt={"pas d'image affiché pour " + pokemon.name} /> : <p>???</p>} */}
+    //         </div>
+    //       ))}
+    //     </figure>
+    //   );
+
 }
 
 export default PokemonCard;
